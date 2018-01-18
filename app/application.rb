2f5746491421f622bc60b014ad511b433379d8e0
Brittany Morris <brittmmendez@gmail.com>
2018-01-18 15:21:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,13 +21,7 @@ class Application
     elsif req.path.match(/add/)
 
       search_term = req.params["item"]
-
-        if @@items.include?(search_term)
-          @@cart << search_term
-          resp.write "added #{search_term} "
-        else
-          resp.write "We don't have that item"
-        end
+      resp.write add_item
 
     else
       resp.write "Path Not Found"
@@ -54,4 +48,13 @@ class Application
     end
   end
 
+def add_item
+  if @@items.include?(search_term)
+    @@cart << search_term
+  return "added #{search_term} "
+  else
+    return "We don't have that item"
+  end
+  
+end
 end
